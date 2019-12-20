97a877725df8cd3ef8104df6b2248e7c1eecd8d2
airuchen <b10307115@gapps.ntust.edu.tw>
2019-12-20 17:03:53 +0800

    Activated double executors

--- neuronbot2_bringup/src/main.cpp
+++ neuronbot2_bringup/src/main.cpp
@@ -12,8 +12,8 @@ int main(int argc, char *argv[])
     auto neuron_base = std::make_shared<NeuronBase>();
     executor.add_node(neuron_base);
 
-    // auto neuron_serial = std::make_shared<NeuronSerial>();
-    // executor.add_node(neuron_serial);
+    auto neuron_serial = std::make_shared<NeuronSerial>();
+    executor.add_node(neuron_serial);
 
 
     executor.spin();
