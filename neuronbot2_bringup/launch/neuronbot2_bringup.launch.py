af6d3d940b7271a4176943b3a6fe26a76cb8ac6c
airuchen <b10307115@gapps.ntust.edu.tw>
2019-12-24 10:57:02 +0800

    Sensor fusion with ekf

--- neuronbot2_bringup/launch/neuronbot2_bringup.launch.py
+++ neuronbot2_bringup/launch/neuronbot2_bringup.launch.py
@@ -13,8 +13,10 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='False')
     description_launch_file_dir = os.path.join(get_package_share_directory('neuronbot2_gazebo'), 'launch')
 
+    ekf_config = Path(get_package_share_directory('neuronbot2_bringup'), 'cfg', 'ekf.yaml')
     urdf_path = Path(get_package_share_directory('neuronbot2_description'), 'urdf', 'neuronbot2.urdf')
-    hardware_config = Path(get_package_share_directory('omni_ros2'), 'config', 'hardware.yaml')
+    hardware_config = Path(get_package_share_directory('neuronbot2_bringup'), 'cfg', 'hardware.yaml')
+    # hardware_config = Path(get_package_share_directory('omni_ros2'), 'cfg', 'hardware.yaml')
     assert hardware_config.is_file()
 
     return LaunchDescription([
@@ -40,7 +42,7 @@ def generate_launch_description():
             output='screen',
             arguments=[str(urdf_path)],
             parameters=[hardware_config]
-            ),
+        ),
 
 
         Node(
@@ -48,5 +50,15 @@ def generate_launch_description():
             node_executable='neuronbot2_driver',
             node_name='neuronbot2_driver',
             # output='screen',
-            ),
+
+            # parameters=[{"calibrate_imu": False}]
+        ),
+
+        Node(
+            package='robot_localization',
+            node_executable='se_node',
+            output='screen',
+            parameters=[ekf_config],
+            remappings=[("odometry/filtered", "odom")]
+        ),
     ])
