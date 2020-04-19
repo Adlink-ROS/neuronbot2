d6510bfcc5b84b36bdb32246809bf2e9842a634c
Ting Chang <qting0529@hotmail.com>
2020-04-20 01:41:43 +0800

    Friendly launch file to run RVIZ2 with sim_time.

---
+++ neuronbot2_nav/launch/rviz_w_sim_time.launch.py
@@ -0,0 +1,26 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.conditions import IfCondition
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    rviz_config_dir = os.path.join(
+            get_package_share_directory('neuronbot2_nav'),
+            'rviz',
+            'nav2.rviz')
+
+    return LaunchDescription([
+        Node(
+            package='rviz2',
+            node_executable='rviz2',
+            node_name='rviz2',
+            arguments=['-d', rviz_config_dir],
+            parameters=[{'use_sim_time':'true'}],
+            ),
+        ])
