876ffdae002662188fc1d63ba887465a48209f51
ChenYing Kuo <evshary@gmail.com>
2020-05-31 15:59:58 +0800

    Rename name and executable in node.

--- neuronbot2_nav/launch/neuronbot2_nav.launch.py
+++ neuronbot2_nav/launch/neuronbot2_nav.launch.py
@@ -83,8 +83,8 @@ def generate_launch_description():
 
         Node(
             package='rviz2',
-            node_executable='rviz2',
-            node_name='rviz2',
+            executable='rviz2',
+            name='rviz2',
             arguments=['-d', rviz_config_dir],
             parameters=[{'use_sim_time': use_sim_time}],
             condition=IfCondition(LaunchConfiguration("open_rviz"))
