adaf37021a8b3e1ca3711ce2e32a94b9a88ee061
Ting Chang <qting0529@hotmail.com>
2020-07-30 17:54:09 +0800

    change LED to blue when bringup

--- neuronbot2_tools/neuronbot2_led/scripts/led_node.py
+++ neuronbot2_tools/neuronbot2_led/scripts/led_node.py
@@ -5,33 +5,41 @@ import sys
 from std_msgs.msg import Byte
 from std_msgs.msg import UInt16
 
-def led_loop(led_color):
-	rospy.init_node('led_node')
-	pub1 = rospy.Publisher('table_command', Byte, queue_size = 10)
-	r = rospy.Rate(30)
+class led_ctrl_node:
+	def __init__(self):
+		self.LED_BLACK = 0
+		self.LED_WHITE = 1
+		self.LED_AMBER = 2
+		self.LED_BLUE = 3
+		self.LED_GREEN = 4
+		self.LED_RED = 5
+		self.LED_METEOR = 6
+		self.LED_RAINBOW = 7
+		self.LED_RAINBOW_BREATHING = 8
+		self.LED_BREATHING = 9
+		self.LED_BACK_AND_FORWARD = 10
+		rospy.init_node('led_node')
+		self.rate = rospy.Rate(30)
+		self.pub = rospy.Publisher('table_command', Byte, queue_size = 10)
+		rospy.on_shutdown(self.onclosing)
 
-	if len(led_color) > 0:
-		command = int(led_color)
-	else:
-		command = 0		
+	def led_loop(self, led_color):
+		if len(led_color) > 0:		
+			command = int(led_color)
+		else:
+			command = self.LED_BLACK		
 
-	while not rospy.is_shutdown():
-		pub1.publish(command)
-		r.sleep()
+		while not rospy.is_shutdown():
+			self.pub.publish(command)
+			self.rate.sleep()
+
+	def onclosing(self):
+		print >> sys.stderr, "shutdown time!"
+		self.pub.publish(self.LED_AMBER)
+
 
 #------------------------------Main-------------------------------#
-if __name__ == '__main__':		
-	# LED color
-	# '0': Black
-	# '1': Amber
-	# '2': Blue
-	# '3': Green
-	# '4': Red
-	# '5': Meteor
-	# '6': Rainbow
-	# '7': Rainbow breathing
-	# '8': Breathing
-	# '9': Back and Forward	
-	# print >> sys.stderr, "led_color=%s" % led_color	
-	led_color = rospy.get_param('/led_control_node/led_color', '7')
-	led_loop(led_color)
+if __name__ == '__main__':
+	node = led_ctrl_node()
+	led_color = rospy.get_param('/led_control_node/led_color', '3')	
+	node.led_loop(led_color)
