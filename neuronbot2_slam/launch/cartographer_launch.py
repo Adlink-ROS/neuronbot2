4b0410561a0531579b89486b9123a00cbf1bf247
shwnyao <shawn.swyao@gmail.com>
2021-07-09 10:56:01 +0800

    feat(cartographer): allow cartographer to run under namespace

--- neuronbot2_slam/launch/cartographer_launch.py
+++ neuronbot2_slam/launch/cartographer_launch.py
@@ -1,24 +1,33 @@
 import os
 from  ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import SetEnvironmentVariable
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, SetEnvironmentVariable
 from launch.conditions import IfCondition
+from nav2_common.launch import ReplaceString
 
 
 def generate_launch_description():
+    namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time', default='False')
     cartographer_config_dir = LaunchConfiguration('cartographer_config_dir', 
         default=os.path.join(get_package_share_directory('neuronbot2_slam') , 'config'))
     configuration_basename = LaunchConfiguration('configuration_basename', default='cartographer.lua')
     resolution = LaunchConfiguration('resolution', default='0.05')
     publish_period_sec = LaunchConfiguration('publish_period_sec', default='0.5')
-    rviz_config_dir = os.path.join(get_package_share_directory('neuronbot2_slam'), 'rviz', 'slam.rviz')
+    rviz_config_file = os.path.join(get_package_share_directory('neuronbot2_slam'), 'rviz', 'namespaced_slam.rviz')
 
+    namespaced_rviz_config_file = ReplaceString(
+            source_file=rviz_config_file,
+            replacements={'<robot_namespace>': namespace})
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'namespace',
+            default_value='robot0',
+            description='namespace'),
+
         DeclareLaunchArgument(
             'open_rviz',
             default_value='false',
@@ -45,11 +54,15 @@ def generate_launch_description():
             default_value=publish_period_sec,
             description='OccupancyGrid publishing period'),
 
+        SetEnvironmentVariable(
+            name='ROBOT_NAMESPACE', 
+            value=namespace),
 
         Node(
             package='cartographer_ros',
             executable='cartographer_node',
             name='cartographer_node',
+            namespace=namespace,
             output='screen',
             parameters=[{'use_sim_time': use_sim_time}],
             arguments=[
@@ -60,6 +73,7 @@ def generate_launch_description():
             package='cartographer_ros',
             executable='occupancy_grid_node',
             name='occupancy_grid_node',
+            namespace=namespace,
             output='screen',
             parameters=[{'use_sim_time': use_sim_time}],
             arguments=['-resolution', resolution, '-publish_period_sec', publish_period_sec]),
@@ -68,7 +82,8 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            arguments=['-d', rviz_config_dir],
+            namespace=namespace,
+            arguments=['-d', namespaced_rviz_config_file],
             parameters=[{'use_sim_time': use_sim_time}],
             condition=IfCondition(LaunchConfiguration("open_rviz")),
             output='screen'),
