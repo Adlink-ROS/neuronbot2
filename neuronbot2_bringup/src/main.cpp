076008a6ff391f24288e9df4a9564c1dd43a96f6
Ting Chang <ting.chang@adlinktech.com>
2021-07-26 11:30:52 +0800

    LED turns blue when bringup, turns amber when shutdown

--- neuronbot2_bringup/src/main.cpp
+++ neuronbot2_bringup/src/main.cpp
@@ -3,11 +3,15 @@
 
 int main(int argc, char *argv[])
 {
+    system("/home/ros/neuronbot2_ros1_ws/src/neuronbot2/neuronbot2_tools/neuronbot2_led/scripts/led_control.py --port /dev/neuronbotLED --mode 5");
+
     ros::init(argc, argv, "neuronbot2_driver");
     
     BaseDriver::Instance()->work_loop();
 
     ros::spin();
+
+    system("/home/ros/neuronbot2_ros1_ws/src/neuronbot2/neuronbot2_tools/neuronbot2_led/scripts/led_control.py --port /dev/neuronbotLED --mode 2");
 
     return 0;
 }
